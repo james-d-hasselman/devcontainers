176d199eeb2b9f1eb5e2006fdcfaee2837c0f44f
James D. Hasselman <james.d.hasselman@gmail.com>
2025-03-23 22:05:03 -0400

    Correct path for environment file

--- src/tauri-2.0/.devcontainer/createEnvironmentFile.sh
+++ src/tauri-2.0/.devcontainer/createEnvironmentFile.sh
@@ -2,9 +2,9 @@
 IFS=: read _1 _2 VIDEO_GROUP <<< `getent group video`
 IFS=: read _1 _2 RENDER_GROUP <<< `getent group render`
 
-echo "RUST_VERSION=${1}" > .env
-echo "NVM_VERSION=${2}" >> .env
-echo "NODE_VERSION=${3}" >> .env
-echo "VIDEO_GROUP=${VIDEO_GROUP}" >> .env
-echo "RENDER_GROUP=${RENDER_GROUP}" >> .env
-echo "USER_ID=$(id -u)" >> .env
+echo "RUST_VERSION=${1}" > .devcontainer/.env
+echo "NVM_VERSION=${2}" >> .devcontainer/.env
+echo "NODE_VERSION=${3}" >> .devcontainer/.env
+echo "VIDEO_GROUP=${VIDEO_GROUP}" >> .devcontainer/.env
+echo "RENDER_GROUP=${RENDER_GROUP}" >> .devcontainer/.env
+echo "USER_ID=$(id -u)" >> .devcontainer/.env
